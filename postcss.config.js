87d88bc9d1d0608c351a227cc9e5ef20635383ea
kuminson <273423480@qq.com>
2020-09-28 15:20:12 +0800

    增加 移动端适配 解决方案

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   plugins: [
-    require('autoprefixer')
+    require('autoprefixer'),
+    require('postcss-px-to-viewport')({
+      viewportWidth: 375,
+      mediaQuery: true
+    }),
+    require('css-mqpacker'), // 把重复的@media合成一个
   ]
 }
