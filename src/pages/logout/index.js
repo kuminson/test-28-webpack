e37d29bfa9f64de5eab95e33bff2d72584b52071
kuminson <273423480@qq.com>
2020-11-20 17:21:57 +0800

    增加 prettier配置

--- src/pages/logout/index.js
+++ src/pages/logout/index.js
@@ -1,10 +1,17 @@
 import './index.scss'
+
 console.log('logout')
 
+console.log(
+  new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('Promise')
+      resolve()
+    }, 3000)
+  })
+)
 
-console.log(new Promise(resolve => {
-  setTimeout(() => {
-    console.log('Promise')
-    resolve()
-  }, 3000)
-}))
+const ll = {
+  ak: 123,
+  adsf: 13123
+}
